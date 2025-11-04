be90e965024d8c6283c51f477d4c69e49a79d625
unknown <ysenpai785 @gmail.com>
2025-11-04 14:16:03 +0700

    dependencies

---
+++ lib/pages/zakat_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ZakatPages extends StatelessWidget {
+  const ZakatPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
