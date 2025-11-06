3a6a54239c8434fa98809a1caf682a2539489ec2
unknown <ysenpai785 @gmail.com>
2025-11-06 09:05:05 +0700

    Belajar Carousel

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:async'; //timer coutdown
+import 'package:flutter/material.dart'; //courosel slider
+import 'package:carousel_slider/carousel_slider.dart'; //ambil data
+import 'package:http/http.dart' as http; // ambil data API JSON
+import 'dart:convert'; //decode JSON
+import 'package:geolocator/geolocator.dart'; //GPS
+import 'package:geocoding/geocoding.dart'; //Konversi GPS
+import 'package:intl/intl.dart'; //formater number
+import 'package:permission_handler/permission_handler.dart'; // Izin handler
+import 'package:shared_preferences/shared_preferences.dart'; // cache lokal
+import 'package:string_similarity/string_similarity.dart'; //fuzyy match string
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final CarouselController _controller = CarouselController();
+  int _curentIndex = 0;
+
   final posterList = const <String>[
-    
+    'assets/images/ramadhan.png',
+    'assets/images/idulfitri.png',
+    'assets/images/iduladha.png',
   ];
 
   @override
@@ -119,9 +139,26 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            _buildCarouselSection(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCarouselSection() {
+    return Column(
+      children: [
+        const SizedBox(height: 20),
+        CarouselSlider.builder(
+          itemCount: posterList.length,
+          itemBuilder: (context, index, realindex) {
+            final poster = posterList[index];
+            return Container(child: Image.asset (poster));
+          },
+          options: CarouselOptions(),
+        ),
+      ],
     );
   }
 }
